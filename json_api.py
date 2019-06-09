610c346fa995e29017cbe207c9846bbe20c0ba66
Alexander Luisi <al3515@ic.ac.uk>
2019-06-09 17:16:44 +0100

    first commit

---
+++ json_api.py
@@ -0,0 +1,47 @@
+# Adapted for use from: https://codeburst.io/this-is-how-easy-it-is-to-create-a-rest-api-8a25122ab1f3, accessed 09/06/2019
+
+import json
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+
+app = Flask(__name__)
+api = Api(app)
+
+# Need to figure out how to store the data
+
+data = [
+    {
+        "airQualityEstimate": 1,
+        "latitude": 0,
+        "longitude": 0
+    }
+]
+
+class PollutionData(Resource):
+
+    def get(self):
+        jsonString = json.dumps(data) # Write data to string in memory
+
+        return jsonString, 200
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("airQualityEstimate")
+        parser.add_argument("latitude")
+        parser.add_argument("longitude")
+        args = parser.parse_args()
+
+        pollutionPoint = {
+            "airQualityEstimate" : args["airQualityEstimate"],
+            "latitude" : args["latitude"],
+            "longitude": args["longitude"]
+        }
+
+        data.append(pollutionPoint)
+        jsonString = json.dumps(data) # Write data to string in memory
+        return jsonString, 201
+
+
+
+api.add_resource(PollutionData, "/data")
+app.run(debug=True)
